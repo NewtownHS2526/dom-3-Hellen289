0e7915dc8d498c10a25096394c01aa618004d875
Hellen Arita <hellena3@nycstudents.net>
2026-01-06 14:40:19 -0500

    Still-working

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 let cart= [];
 let total = 0;
-const 
+
+const stickerButton = document.querySelector("#sticker");
+const keychainButton = document.querySelector("#keychain");
+const miniplushButton = document.querySelector("#plush");
+const comicbookButton = document.querySelector("#comic");
+const mysteryboxButton = document.querySelector("#mystery");
+
+const shoppingCart = document.querySelector("#shopping-cart");
+const totalSpan = document.querySelector("#total-Span");
+
+const updateTotal = () => {
+    totalSpan.textContent = totalInCent/100;
+}
+const addSticker = () => {
+    shoppingCart.innerHTML +=
+    totalInCent += 50;
+    updateTotal();
+}
+const addKeychain = () => {
+    shoppingCart.innerHTML +=
+    totalInCent += 150;
+    updateTotal();
+}
+const addPlush = () =>{
+    shoppingCart.innerHTML +=
+    totalInCent += 400;
+    updateTotal();
+}
