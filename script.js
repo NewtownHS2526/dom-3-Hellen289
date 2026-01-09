2056e3b6c1b16dcb1ebbdd234453cfc03e17bd9c
Hellen Arita <hellena3@nycstudents.net>
2026-01-09 13:54:11 -0500

    Still-working

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let cart= [];
 let total = 0;
 
+const products = [
+    {id: 1, name: 'sticker', price: 0.50},
+    {id: 2, name: 'keychain', price: 1.50},
+    {id: 3, name: 'plush', price: 4.00},
+    {id: 2, name: 'comic', price: 2.75},
+    {id: 2, name: 'mystery', price: 3.25},
+]
+
 const stickerButton = document.querySelector("#sticker");
 const keychainButton = document.querySelector("#keychain");
 const miniplushButton = document.querySelector("#plush");
@@ -10,34 +18,34 @@ const mysteryboxButton = document.querySelector("#mystery");
 const shoppingCart = document.getElementById('shopping-cart');
 const totalSpan = document.getElementById('total-span');
 
-const UpdateDisplayTotal = () => {
+const UpdateTotal = () => {
     totalSpan.textContent = total/100;
-    
+    shoppingCart.textContent = stickerButton;
 }
 const addSticker = () => {
     shoppingCart.innerHTML +=
     total += 50;
-    UpdateDisplayTotal();
+    UpdateTotal();
 }
 const addKeychain = () => {
     shoppingCart.innerHTML +=
     total += 150;
-    UpdateDisplayTotal();
+    UpdateTotal();
 }
 const addPlush = () =>{
     shoppingCart.innerHTML +=
     total += 400;
-    UpdateDisplayTotal();
+    UpdateTotal();
 }
 const addComic = () => {
     shoppingCart.innerHTML +=
     total += 275;
-    UpdateDisplayTotal();
+    UpdateTotal();
 }
 const addMystery = () => {
     shoppingCart.innerHTML +=
     total += 325;
-    UpdateDisplayTotal();
+    UpdateTotal();
 }
 
 stickerButton.addEventListener("click", addSticker);
